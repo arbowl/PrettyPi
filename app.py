df20e4804c99cbde77e0e711d386b45504c11bce
Drew B <103056414+arbowl@users.noreply.github.com>
2024-05-04 22:51:39 -0400

    Update for Python 3.12, fix minor visuals

---
+++ app.py
@@ -0,0 +1,222 @@
+from flask import Flask, render_template, request, make_response, redirect, Response
+from hashlib import md5
+from sqlite3 import connect, Connection, Cursor
+from time import strftime
+from typing import Optional
+from user import User
+
+app = Flask(__name__)
+
+
+def get_connection() -> tuple[Connection, Cursor]:
+    connection = connect("data.db")
+    cursor = connection.cursor()
+    return connection, cursor
+
+
+def is_installed() -> bool:
+    _, cursor = get_connection()
+    cursor.execute("SELECT COUNT( 1 ) FROM USERS")
+    has_user = cursor.fetchone()[0]
+    return has_user > 0
+
+
+def add_request(request_type) -> None:
+    connection, cursor = get_connection()
+    creation_date = strftime("%d-%m-%Y %H:%M:%S")
+    cursor.execute(
+        "INSERT INTO UPDATE_REQUESTS( UPDATE_TYPE, DONE, CREATION_DATE ) VALUES"
+        " ( ?, 'N', ? )",
+        (request_type, creation_date),
+    )
+    connection.commit()
+
+
+@app.before_request
+def before_request() -> Optional[str]:
+    if request.path == "/install":
+        return None
+    if not is_installed():
+        return render_template("install_prettypi.html")
+    if request.path == "/login":
+        return None
+    if request.cookies.get("prettypi_username") is not None:
+        User.set_username(request.cookies.get("prettypi_username"))
+        User.set_hashed_password(request.cookies.get("prettypi_password"))
+        return None if User.has_permission() else render_template("login.html")
+    return render_template("login.html")
+
+
+@app.route("/")
+def main() -> str:
+    return render_template("main_page.html", name=User.get_name())
+
+
+@app.route("/install", methods=["POST"])
+def install() -> str:
+    connection, cursor = get_connection()
+    if is_installed():
+        return render_template(
+            "installer_message.html",
+            message="PrettyPi Already Installed",
+            type="danger",
+        )
+    if (
+        not request.form["username"]
+        or not request.form["password"]
+        or not request.form["name"]
+    ):
+        return render_template(
+            "installer_message.html", message="Please fill all fields", type="danger"
+        )
+    hash_function = md5(request.form["password"].encode("utf-8"))
+    hashed_password = hash_function.hexdigest()
+    cursor.execute(
+        "INSERT INTO USERS( USER_ID, USERNAME, PASSWORD, NAME ) VALUES ( NULL, ?, ?, ? )",
+        [request.form["username"], hashed_password, request.form["name"]],
+    )
+    connection.commit()
+    return render_template(
+        "installer_message.html",
+        message="Congratulations! PrettyPi has been initialized. "\
+        "Go back to homepage to start using it",
+        type="success",
+    )
+
+
+@app.route("/login", methods=["POST"])
+def login() -> str:
+    User.set_username(request.form["username"])
+    User.set_password(request.form["password"].encode("utf-8"))
+    if User.has_permission():
+        response = make_response(redirect("/"))
+        response.set_cookie("prettypi_username", User.get_username())
+        response.set_cookie("prettypi_password", User.get_hashed_password())
+        return response
+    return "Invalid"
+
+
+@app.route("/logout")
+def logout() -> Response:
+    response = make_response(redirect("/"))
+    response.set_cookie("prettypi_username", "")
+    response.set_cookie("prettypi_password", "")
+    return response
+
+
+@app.route("/tasks")
+def tasks_main():
+    _, cursor = get_connection()
+    cursor.execute("SELECT * FROM TODO WHERE DONE = 'N' ORDER BY CREATION_DATE DESC")
+    tasks = cursor.fetchall()
+    cursor.execute("SELECT * FROM TODO WHERE DONE = 'Y' ORDER BY CREATION_DATE DESC")
+    done_tasks = cursor.fetchall()
+    cursor.execute("SELECT * FROM TODO WHERE WORKING_ON = 'Y'")
+    working_task = cursor.fetchone()
+    return render_template(
+        "tasks.html",
+        tasks=tasks,
+        done_tasks=done_tasks,
+        name=User.get_name(),
+        working_task=working_task,
+    )
+
+
+@app.route("/add_task", methods=["POST"])
+def new_task():
+    connection, cursor = get_connection()
+    if not request.form["task_details"]:
+        return render_template(
+            "message.html", message="The details should not be empty", type="warning"
+        )
+    creation_date = strftime("%d-%m-%Y %H:%M:%S")
+    cursor.execute(
+        "INSERT INTO TODO( TASK_ID, TASK, CREATION_DATE ) VALUES ( NULL, ?, ? )",
+        [request.form["task_details"], creation_date],
+    )
+    connection.commit()
+    add_request("UPDATE_TODO_LIST")
+    return redirect("tasks")
+
+
+@app.route("/delete_task", methods=["GET"])
+def delete_task() -> Response | str:
+    connection, cursor = get_connection()
+    task_id = request.args.get("task_id", None)
+    if task_id is None:
+        return "Invalid"
+    cursor.execute("DELETE FROM TODO WHERE TASK_ID = ?", [task_id])
+    cursor.execute("DELETE FROM TASKS_LOG WHERE TASK_ID = ?", [task_id])
+    connection.commit()
+    add_request("UPDATE_TODO_LIST")
+    return redirect("tasks")
+
+
+@app.route("/task_done", methods=["GET"])
+def mark_task_as_done() -> Response | str:
+    connection, cursor = get_connection()
+    task_id = request.args.get("task_id", None)
+    if task_id is None:
+        return "Invalid"
+    current_date = strftime("%d-%m-%Y %H:%M:%S")
+    cursor.execute(
+        "UPDATE TODO SET WORKING_ON = 'N', DONE = 'Y', DONE_AT = ? WHERE TASK_ID = ?",
+        [current_date, task_id],
+    )
+    connection.commit()
+    add_request("UPDATE_TODO_LIST")
+    return redirect("tasks")
+
+
+@app.route("/start_task", methods=["GET"])
+def start_working_on_task() -> Response | str:
+    connection, cursor = get_connection()
+    task_id = request.args.get("task_id", None)
+    if task_id is None:
+        return "Invalid"
+    cursor.execute(
+        "SELECT COUNT( 1 ) FROM TODO WHERE WORKING_ON = 'Y' AND TASK_ID <> ?",
+        [task_id]
+    )
+    currently_working_on = cursor.fetchone()[0]
+    if currently_working_on > 0:
+        return render_template(
+            "message.html",
+            message="You're already working on another task!",
+            type="warning",
+        )
+    creation_date = strftime("%d-%m-%Y %H:%M:%S")
+    cursor.execute("UPDATE TODO SET WORKING_ON = 'Y' WHERE TASK_ID = ?",[task_id])
+    cursor.execute(
+        "INSERT INTO TASKS_LOG( TASK_ID, START_AT ) VALUES ( ?, ? )",
+        [task_id, creation_date],
+    )
+    connection.commit()
+    add_request("UPDATE_TODO_LIST")
+    return redirect("tasks")
+
+
+@app.route("/stop_task", methods=["GET"])
+def stop_working_on_task() -> Response | str:
+    connection, cursor = get_connection()
+    task_id = request.args.get("task_id", None)
+    if task_id is None:
+        return "Invalid"
+    cursor.execute(
+        "SELECT MAX( log_id ) FROM tasks_log WHERE TASK_ID = ? AND ENDED_AT IS NULL",
+        [task_id],
+    )
+    current_task_log_id = cursor.fetchone()[0]
+    current_date = strftime("%d-%m-%Y %H:%M:%S")
+    cursor.execute("UPDATE TODO SET WORKING_ON = 'N' WHERE TASK_ID = ?", [task_id])
+    cursor.execute(
+        "UPDATE TASKS_LOG SET ENDED_AT = ? WHERE LOG_ID = ?",
+        [current_date, current_task_log_id],
+    )
+    connection.commit()
+    add_request("UPDATE_TODO_LIST")
+    return redirect("tasks")
+
+
+if __name__ == "__main__":
+    app.debug = True
---
+++ main.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# [MQH] 16 June 2017. It has been a while since last code I have written in Python! :-)
+from kivy.app import App
+from kivy.lang import Builder
+from kivymd.theming import ThemeManager
+from kivy.properties import ObjectProperty
+from kivymd.label import MDLabel
+from kivy.animation import Animation
+from ArabicLabel import ArabicLabel
+import arabic_reshaper
+import sqlite3
+import threading
+from kivy.clock import Clock
+
+kv_config = """
+#:import Toolbar kivymd.toolbar.Toolbar
+#:import NavigationLayout kivymd.navigationdrawer.NavigationLayout
+#:import MDNavigationDrawer kivymd.navigationdrawer.MDNavigationDrawer
+
+NavigationLayout:
+	id: navLayout
+
+	MDNavigationDrawer:
+        id: navDrawer
+        NavigationDrawerToolbar:
+            title: "Navigation Drawer"
+        NavigationDrawerIconButton:
+			id: quit_button
+            icon: 'checkbox-blank-circle'
+            text: "Quit"
+
+	BoxLayout:
+		id: topBox
+		orientation: 'vertical'
+		Toolbar:
+			id: toolbar
+			title: 'My Pretty Pi!'
+			md_bg_color: app.theme_cls.primary_color
+			background_palette: 'Primary'
+			background_hue: '500'
+			left_action_items: [['menu', lambda x: app.root.toggle_nav_drawer()]]
+			right_action_items: [['dots-vertical', lambda x: app.root.toggle_nav_drawer()]]
+
+		Toolbar:
+			id: titlebar
+			title: 'Current TODO'
+			md_bg_color: app.theme_cls.primary_color
+			background_palette: 'Primary'
+			background_hue: '900'
+
+		ScreenManager:
+			id: screenManager
+
+			Screen:
+				name: 'mainScreen'
+
+				BoxLayout:
+					id: main_box
+					orientation: 'vertical'
+	"""
+
+
+class PrettyPiApp(App):
+    theme_cls = ThemeManager()
+    main_box = None
+    connection = None
+    cursor = None
+    kv_main = kv_config
+
+    def build(self):
+        main_widget = Builder.load_string(self.kv_main)
+        self.connection = sqlite3.connect("data.db")
+        self.cursor = self.connection.cursor()
+        self.main_box = main_widget.ids.mainBox
+        self.quit_button = main_widget.ids.quitBtn
+        self.quit_button.bind(on_press=lambda e: exit())
+        self.refresh_list()
+        Clock.schedule_interval(self.check_updates, 0.5)
+        return main_widget
+
+    def refresh_list(self) -> None:
+        self.main_box.clear_widgets()
+        self.cursor.execute("SELECT * FROM TODO WHERE DONE = 'N'")
+        tasks = self.cursor.fetchall()
+        for task in tasks:
+            task_text = task[1]
+            if task[5] == "Y":
+                task_text += " (Working On)"
+            self.main_box.add_widget(
+                ArabicLabel(text=task_text, halign="center", font_style="Display1")
+            )
+
+    def check_updates(self) -> None:
+        self.cursor.execute(
+            "SELECT COUNT( 1 ) FROM UPDATE_REQUESTS WHERE UPDATE_TYPE = "\
+            "'UPDATE_TODO_LIST' AND DONE = 'N'"
+        )
+        result = self.cursor.fetchone()
+        if result[0] > 0:
+            self.refresh_list()
+            self.cursor.execute(
+                "UPDATE UPDATE_REQUESTS SET DONE = 'Y' WHERE UPDATE_TYPE = "\
+                "'UPDATE_TODO_LIST'"
+            )
+            self.connection.commit()
+
+
+if __name__ == "__main__":
+    PrettyPiApp().run()
